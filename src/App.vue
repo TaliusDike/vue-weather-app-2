ab525f521920115a3cbb929e5134c8b536402561
Arinze Stanley Ebeledike <ebeledikea@gmail.com>
2023-02-23 20:59:39 +0100

    Created a simple weather app using Vue JS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" >
+    <WeatherApp/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import WeatherApp from './components/WeatherApp.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    WeatherApp
   }
+ 
 }
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box
+ }
+
+ body {
+  font-family: 'montserrat', san-serif;
+ }
+
+ /* .main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75) );
+ } */
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
   margin-top: 60px;
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
 }
+
+#app .warm {
+  background-image: url('./assets/warm-bg.jpg');
+}
+
 </style>
